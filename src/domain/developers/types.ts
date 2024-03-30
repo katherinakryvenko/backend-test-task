bec8f2a33cae6080d7d4f4747ed2c466454fe5cb
Kate Kryvenko <katherinakryvenko@gmail.com>
2024-03-30 21:28:28 +0200

    added a type for contracts, extended IDeveloper with a revenue property

--- src/domain/developers/types.ts
+++ src/domain/developers/types.ts
@@ -1,10 +1,20 @@
 export interface IDeveloper {
+  id: string;
 
-	id: string
+  firstName?: string;
+  lastName?: string;
 
-	firstName?: string
-	lastName?: string
+  email: string;
 
-	email: string
+  revenue?: number;
+}
 
+export interface IContract {
+  id: number;
+
+  developerId: string;
+
+  status: "pending" | "completed" | "ongoing";
+
+  amount: number;
 }
