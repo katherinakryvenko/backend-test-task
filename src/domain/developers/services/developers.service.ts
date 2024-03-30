dabf25818455b94cd341bc666b1e01c0bc18d6c7
Kate Kryvenko <katherinakryvenko@gmail.com>
2024-03-30 21:49:55 +0200

    extended business logic to calculate and include revenue by contracts

--- src/domain/developers/services/developers.service.ts
+++ src/domain/developers/services/developers.service.ts
@@ -1,20 +1,38 @@
-import { inject, injectable } from 'inversify';
-import { DevelopersRepository } from '../repositories/developers.repository';
-import { IDeveloper } from '../types'
+import { inject, injectable } from "inversify";
+import { DevelopersRepository } from "../repositories/developers.repository";
+import { IDeveloper } from "../types";
+import _ from "lodash";
 
 @injectable()
 export class DevelopersService {
+  constructor(
+    @inject("DevelopersRepository")
+    private developersRepository: DevelopersRepository
+  ) {}
 
-	constructor(
-		@inject('DevelopersRepository') private developersRepository: DevelopersRepository,
-	) {}
+  async getDevelopers(): Promise<IDeveloper[]> {
+    const developers = await this.developersRepository.getDevelopers();
+    return Promise.all(
+      developers.map(async (developer) => {
+        const revenue = await this.calculateDeveloperRevenue(developer.id);
+        return { ...developer, revenue };
+      })
+    );
+  }
 
-	async getDevelopers(): Promise<IDeveloper[]>{
-		return this.developersRepository.getDevelopers()
-	}
+  async getDeveloperById(id: string) {
+    const developer = await this.developersRepository.getDeveloperById(id);
+    const revenue = await this.calculateDeveloperRevenue(developer.id);
+    return { ...developer, revenue };
+  }
 
-	async getDeveloperById(id: string){
-		return this.developersRepository.getDeveloperById(id)
-	}
-
+  private async calculateDeveloperRevenue(
+    developerId: string
+  ): Promise<number> {
+    const contracts = await this.developersRepository.getContractsByDeveloperId(
+      developerId
+    );
+    const completedContracts = _.filter(contracts, { status: "completed" });
+    return _.sumBy(completedContracts, "amount");
+  }
 }
