bc845acacc734468bf95a4afab3c4193fc395537
Kate Kryvenko <katherinakryvenko@gmail.com>
2024-03-30 21:30:38 +0200

    assigned IContract type where it's missing

--- src/domain/developers/repositories/developers.repository.ts
+++ src/domain/developers/repositories/developers.repository.ts
@@ -2,23 +2,21 @@
 // Репозиторій імітує шар підключення до бази данних. Данні знаходяться в data.ts
 // **************************************************************************
 
-import { injectable } from 'inversify';
-import { IDeveloper } from '../types'
-import { contracts, developers } from './data'
+import { injectable } from "inversify";
+import { IContract, IDeveloper } from "../types";
+import { contracts, developers } from "./data";
 
 @injectable()
 export class DevelopersRepository {
+  async getDevelopers(): Promise<IDeveloper[]> {
+    return developers;
+  }
 
-	async getDevelopers(): Promise<IDeveloper[]>{
-		return developers
-	}
+  async getDeveloperById(id: string): Promise<IDeveloper> {
+    return developers.find((d) => d.id === id);
+  }
 
-	async getDeveloperById(id: string): Promise<IDeveloper>{
-		return developers.find(d => d.id === id)
-	}
-
-	async getContracts(){
-		return contracts
-	}
-
+  async getContracts(): Promise<IContract[]> {
+    return contracts;
+  }
 }
