f9bf5394c9598e12297cb0877ff24494a33508cd
Kate Kryvenko <katherinakryvenko@gmail.com>
2024-04-01 02:05:19 +0300

    added support for extended version of the developer on the endpoints with a query param

--- src/rest/controllers/developers.controller.ts
+++ src/rest/controllers/developers.controller.ts
@@ -1,35 +1,55 @@
 import {
-	interfaces,
-	controller,
-	BaseHttpController,
-	httpGet, requestParam
-} from 'inversify-express-utils';
-import { ApiOperationGet, ApiPath } from 'swagger-express-ts';
+  interfaces,
+  controller,
+  BaseHttpController,
+  httpGet,
+  requestParam,
+  queryParam,
+} from "inversify-express-utils";
+import { ApiOperationGet, ApiPath } from "swagger-express-ts";
 import {
-	path, getDevelopers, getDeveloperById
-} from '../swagger/developers.swagger.docs';
-import { inject } from 'inversify'
-import { DevelopersService } from '../../domain/developers/services/developers.service'
-import { DeveloperDto } from '../dto/developers.responses.dto'
+  path,
+  getDevelopers,
+  getDeveloperById,
+} from "../swagger/developers.swagger.docs";
+import { inject } from "inversify";
+import { DevelopersService } from "../../domain/developers/services/developers.service";
+import { DeveloperDto } from "../dto/developers.responses.dto";
 
-@controller('/api/developers')
+@controller("/api/developers")
 @ApiPath(path)
-export class DevelopersController extends BaseHttpController implements interfaces.Controller {
+export class DevelopersController
+  extends BaseHttpController
+  implements interfaces.Controller
+{
+  constructor(
+    @inject("DevelopersService") private developersService: DevelopersService
+  ) {
+    super();
+  }
 
-	constructor(
-		@inject('DevelopersService') private developersService: DevelopersService,
-	){ super() }
+  @httpGet("/")
+  @ApiOperationGet(getDevelopers)
+  public async getDevelopers(
+    @queryParam("includeExtended") includeExtended: string
+  ): Promise<DeveloperDto[]> {
+    if (includeExtended === "true") {
+      return this.developersService.getDevelopersExtended();
+    } else {
+      return this.developersService.getDevelopers();
+    }
+  }
 
-	@httpGet('/')
-	@ApiOperationGet(getDevelopers)
-	public async getDevelopers(): Promise<DeveloperDto[]> {
-		return this.developersService.getDevelopers()
-	}
-
-	@httpGet('/:id')
-	@ApiOperationGet(getDeveloperById)
-	public async getDeveloperById(@requestParam('id') id: string): Promise<DeveloperDto> {
-		return this.developersService.getDeveloperById(id)
-	}
-
+  @httpGet("/:id")
+  @ApiOperationGet(getDeveloperById)
+  public async getDeveloperById(
+    @requestParam("id") id: string,
+    @queryParam("includeExtended") includeExtended: string
+  ): Promise<DeveloperDto> {
+    if (includeExtended === "true") {
+      return this.developersService.getDeveloperByIdExtended(id);
+    } else {
+      return this.developersService.getDeveloperById(id);
+    }
+  }
 }
